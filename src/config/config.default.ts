ba5caea3706cfa69928553987a94a67e82b9b44b
chalee <695469701@qq.com>
2025-04-05 05:41:16 +0800

    feat: init project

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1743802269950_3391',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
